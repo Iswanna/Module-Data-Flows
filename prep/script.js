5c497c85d891a126e1f302bc36527510574bef11
iswat <ibeetek@gmail.com>
2025-11-30 11:58:47 +0000

    Update variable to store an array of objects and add film card creation functions
    - Added function to create a single film card
    - Added loop to generate multiple film cards and append them to the DOM

--- prep/script.js
+++ prep/script.js
@@ -1,21 +1,39 @@
-const films = 
-    {
-      title: "Killing of Flower Moon",
-      director: "Martin Scorsese",
-      times: ["15:35"],
-      certificate: "15",
-      duration: 112,
-    };
+const films = [
+  {
+    title: "Killing of Flower Moon",
+    director: "Martin Scorsese",
+    times: ["15:35"],
+    certificate: "15",
+    duration: 112,
+  },
+  {
+    title: "Typist Artist Pirate King",
+    director: "Carol Morley",
+    times: ["15:00", "20:00"],
+    certificate: "12A",
+    duration: 108,
+  },
+];
+console.log(films, "<------film---->");
 
-    console.log(films, "<------film---->");
-    // The goal is to render a single film card in the browser's user interface
+// The goal is to render a single film card in the browser's user interface
 
-    // Goal: Refactor the code implementation above to use a film card template
+// Goal: Refactor the code implementation above to use a film card template
+// Note: the filmcard component is the function below which is used to create the filmcard in the UI
+function createFilmCard(film) {
+  const filmCard = document
+    .getElementById("film-card-template")
+    .content.cloneNode(true);
 
-    const filmCard = document.getElementById("film-card-template").content.cloneNode(true);
+  filmCard.querySelector("h3").textContent = film.title;
+  filmCard.querySelector("p").textContent = film.director;
+  filmCard.querySelector("time").textContent = film.times;
+  filmCard.querySelector("data").textContent = film.certificate;
 
-    console.log(filmCard);
+  return filmCard;
+}
 
-    filmCard.querySelector("h3").textContent = films.title;
-
-    document.body.appendChild(filmCard);
+for (const film of films) {
+  const card = createFilmCard(film);
+  document.body.append(createFilmCard(film));
+}
