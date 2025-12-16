2704cd4bf1382f10de9d220db06fab440cd1ae56
iswat <ibeetek@gmail.com>
2025-12-16 15:31:49 +0000

    Refactor film card creation into a reusable function

--- prep/practice-1/script.js
+++ prep/practice-1/script.js
@@ -6,23 +6,26 @@ const film = {
     duration: 112,
   };
 
-  function createChildElement(parentElement, tagname, textContent) {
-    const element = document.createElement(tagname);
-    element.textContent = textContent;
-    parentElement.append(element);
-    return element;
-  }
-   // Get a <template> element from the DOM (#film-card)
-    const template = document.getElementById("film-card")
-   // Clone its contents (template.content.cloneNode(true))
-  const card = template.content.cloneNode(true);
-  // Now, we are going to query our cloned fragment, not the entire page.
-  // Fill in text values using data from a film object
-  card.querySelector("h3").textContent = film.title;
-  card.querySelector("[data-director]").textContent = `Director: ${film.director}`;
-  card.querySelector("time").textContent = `${film.duration} minutes`;
-  card.querySelector("[data-certificate").textContent = `Certificate: ${film.certificate}`;
 
-  // Append the cloned content to the page so it becomes visible
-  document.body.append(card);
+
+const createFilmCard = (film) => {
+// Get a <template> element from the DOM (#film-card)
+const template = document.getElementById("film-card")
+// Clone its contents (template.content.cloneNode(true))
+const card = template.content.cloneNode(true);
+// Now, we are going to query our cloned fragment, not the entire page.
+// Fill in text values using data from a film object
+card.querySelector("h3").textContent = film.title;
+card.querySelector("[data-director]").textContent = `Director: ${film.director}`;
+card.querySelector("time").textContent = `${film.duration} minutes`;
+card.querySelector("[data-certificate").textContent = `Certificate: ${film.certificate}`;
+
+return card
+}
+
+// call the createFilmCard function
+const filmCard = createFilmCard(film)
+// Append the cloned content to the page so it becomes visible
+document.body.append(filmCard);
+
 
