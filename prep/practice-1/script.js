29bcddad7111da36716566d1848f2d127e11278d
iswat <ibeetek@gmail.com>
2025-12-18 13:37:37 +0000

    refactor: use map method instead of for...of loop
    Replace manual array iteration with films.map(createFilmCard) for cleaner, more functional approach to generating film cards.

--- prep/practice-1/script.js
+++ prep/practice-1/script.js
@@ -51,15 +51,11 @@ const createFilmCard = ({ title, director, duration, certificate }) => {
 
   return card;
 };
-// define a variable and initialize it with an empty array
-const filmCards = [];
 
-// loop through the array of objects
-for (const film of films) {
-  // for each film, use the createFilmCard function to create a filmcard for the film
-  // push the filmcard created into the filmCards array.
-  filmCards.push(createFilmCard(film));
-}
+// pass the function createFimCard as an argument to the map method so that
+// map method will loop through each film in the films array and implement the function createFimCards on it
+// filmCards variable will holds a reference to the new array with the result
+const filmCards = films.map(createFilmCard);
 
 // Append the array of filmcards to the page so it becomes visible using the spread operator
 document.body.append(...filmCards);
